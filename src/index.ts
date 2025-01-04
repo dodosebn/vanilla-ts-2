029d8443e374b6949e1f898a67fa2c234d827af8
orji Dominion <orjidominion32@gmail.com>
2025-01-04 22:11:14 +0100

    first commit

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,45 @@
-const character = 'martin';
+import { Invoice } from "./classes/Invoice";
+import { Payment } from "./classes/Payment";
+import { ListTemplate } from "./classes/ListTemplate";
+import { HasFormatter } from "./interfaces/HasFormatter";
+const form = document.querySelector('.new-item-form') as HTMLFormElement;
 
-console.log(character);
+// inputs
+const type = document.querySelector('#type') as HTMLInputElement;
+const tofrom = document.querySelector('#tofrom') as HTMLInputElement;
+const details = document.querySelector('#details') as HTMLInputElement;
+const amount = document.querySelector('#amount') as HTMLInputElement;
 
-const inputs = document.querySelectorAll('input');
-console.log(inputs);
+// list template instance
+const ul = document.querySelector('ul')!;
+const list = new ListTemplate(ul);
 
-inputs.forEach(input => {
-  console.log(input);
+form.addEventListener('submit', (e: Event) => {
+  e.preventDefault();
+
+  let values: [string, string, number];
+  values = [tofrom.value, details.value, amount.valueAsNumber];
+
+  let doc: HasFormatter;
+  if (type.value === 'invoice') {
+    doc = new Invoice(...values);
+  } else {
+    doc = new Payment(...values);
+  }
   
-})
+  list.render(doc, type.value, 'end');
+});
+
+// TUPLES
+let arr = ['ryu', 25, true];
+arr[0] = false;
+arr[1] = 'yoshi';
+arr = [30, false, 'yoshi'];
+
+let tup: [string, number, boolean] = ['ryu', 25, true];
+// tup[0] = false;
+tup[0] = 'ken';
+
+let student: [string, number];
+//student = [23564, 'chun-li'];
+student = ['chun-li', 23564];
